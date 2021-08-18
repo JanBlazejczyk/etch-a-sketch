e010f2db8d5cbbd098fc7a6986c71914b772d0e3
Jan Błażejczyk <JB.Blazejczyk@gmail.com>
2021-08-18 11:49:41 +0200

    creates a grid with the given size

---
+++ script.js
@@ -0,0 +1,19 @@
+const sketchArea = document.querySelector("#grid-area");
+
+// takes gridCount (int) - number of rows and pixels in each row
+// creates grid inside of the container
+const createGrid = (gridCount) => {
+    // create a number of rows (specified by gridCount) in the grid
+    for (let i = 1; i <= gridCount; i++) {
+        let row = document.createElement('div');
+        row.classList.add("row");
+        sketchArea.appendChild(row);
+        // for each row append the same number of cells as the number of rows
+        for (let rowNum = 1; rowNum <= gridCount; rowNum++) {
+            let cell = document.createElement("div");
+            cell.classList.add("grid-pixel");
+            row.appendChild(cell);
+        }
+    }
+}
+createGrid(16);
