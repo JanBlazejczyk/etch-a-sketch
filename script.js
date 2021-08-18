2c36e8ec7ca974ab5d3d7da0615c9f4785ca7060
Jan Błażejczyk <JB.Blazejczyk@gmail.com>
2021-08-18 11:52:58 +0200

    made class names more descriptive

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const createGrid = (gridCount) => {
     // create a number of rows (specified by gridCount) in the grid
     for (let i = 1; i <= gridCount; i++) {
         let row = document.createElement('div');
-        row.classList.add("row");
+        row.classList.add("grid-row");
         sketchArea.appendChild(row);
         // for each row append the same number of cells as the number of rows
         for (let rowNum = 1; rowNum <= gridCount; rowNum++) {
             let cell = document.createElement("div");
-            cell.classList.add("grid-pixel");
+            cell.classList.add("grid-cell");
             row.appendChild(cell);
         }
     }
 }
-createGrid(16);
+createGrid(25);
