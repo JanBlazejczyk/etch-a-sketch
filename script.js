3b46fd6a8646f2abb030a860f446b023ea637389
Jan Błażejczyk <JB.Blazejczyk@gmail.com>
2021-08-19 10:13:55 +0200

    resize slider + clear btn

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const createGrid = (gridCount) => {
     }
 }
 
-createGrid(12);
+createGrid(64);
 
 // write a function that listens for hovering and changes the cell color for black
 const cells = document.querySelectorAll('.grid-cell');
@@ -25,15 +25,45 @@ cells.forEach((cell) => cell.addEventListener("mouseenter", function (event) {
     event.target.classList.add('colored');
 }))
 
-// TODO:
 // create a clear button which makes all the cells white again
+const clearBtn = document.querySelector("#clear-btn");
+clearBtn.addEventListener("click", function () {
+    cells.forEach(cell => cell.classList.remove('colored'));
+})
+
+// TODO:
 // create a slider https://www.w3schools.com/howto/howto_js_rangeslider.asp for adjusting the size of the grid
+const gridCountSlider = document.querySelector("#grid-size-slider");
+console.log(gridCountSlider.value);
+let showGridSize = document.querySelector(".slider-value");
+showGridSize.innerHTML = gridCountSlider.value;
+
+gridCountSlider.oninput = function () {
+    showGridSize.innerHTML = this.value;
+}
+
+// make the grid reize to the slider's value when a resize button is clicked
+const resizeBtn = document.querySelector("#resize-btn");
+resizeBtn.addEventListener("click", function () {
+    const sketchArea = document.querySelector("#grid-area");
+    sketchArea.innerHTML = "";
+    createGrid(gridCountSlider.value);
+    // below functionalities does not work once the grid is resized
+    const cells = document.querySelectorAll('.grid-cell');
+    cells.forEach((cell) => cell.addEventListener("mouseenter", function (event) {
+        event.target.classList.add('colored');
+    }))
+    const clearBtn = document.querySelector("#clear-btn");
+    clearBtn.addEventListener("click", function () {
+        cells.forEach(cell => cell.classList.remove('colored'));
+    })
+})
+
 // create a color picker so the user can paint in different colors
-// make the divs chenge collor only when the mouse button is pressed (if it's not pressed just make another background color just on hover - as a coursor)
-// make the mouse dissappera inside the grid area as we no longer need it
+// create another color picker for te background color change
+// make the divs change color only when the draving is activated (by clicking a mouse)
+// make the mouse dissapper inside the grid area as we no longer need it
 // style everything nicely with css so it has an etch a sketch toy look
-
-
 
 
 
